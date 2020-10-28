80d66f0c934d511ddbeb872d5e49514c2fd544e7
Andrew Brown <35886726+Tengoon@users.noreply.github.com>
2020-10-28 15:18:21 -0500

    Add files via upload
    Adding files to fork, hopefully everything looks right.

--- Constants.py
+++ Constants.py
@@ -1,4 +1,10 @@
-class Constants:
-    # Use constants so specfic user input type can be referenced globally without magic strings everywhere.
-    SYMBOL = "symbol"
-    CHARTTYPE = "chartType"
+API_URL = "https://www.alphavantage.co/query"
+API_KEY = "SPFP6JN8KMDZ9IPM"
+#Constants below this comment will be set by user input and currently hold placeholder values.
+#See https://www.alphavantage.co/documentation/#daily on how the function parameters work.
+SYMBOL = "GOOGL"
+#True for Line, False for Bar
+CHART_TYPE = True
+TIME_SERIES = "TIME_SERIES_DAILY"
+START_DATE = "2020-01-01"
+END_DATE = "2020-01-31"
