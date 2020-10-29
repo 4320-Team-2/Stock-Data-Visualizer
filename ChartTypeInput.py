af08b7405a78df271de5ba785f3fbe42dcbe3731
Nilin Reddrex <nilin.reddrex@outlook.com>
2020-10-28 18:48:42 -0700

    Updated chart types to be constant. Fixed bug in input validation.

--- ChartTypeInput.py
+++ ChartTypeInput.py
@@ -1,16 +1,16 @@
 from UserInput import UserInput
+from Constants import Constants
 
 class ChartTypeInput(UserInput):
     
     def __init__(self):
-
         """Chart Type input constructor"""
             
         # The example txt displayed to the user on input request
-        exampletxt =    ("Chart Types\r\n" +
-                        "=============\r\n" +
-                        "1. Bar\r\n" +
-                        "2. Line\r\n")
+        exampletxt = ("Chart Types\r\n" +
+                     "=============\r\n")
+        for key, value in Constants.CHARTTYPES.items():
+            exampletxt += key + ". " + value + "\r\n"
             
         # Call base constructor initalizing prompt message and example txt
         UserInput.__init__(
@@ -18,16 +18,12 @@ class ChartTypeInput(UserInput):
             "Enter the chart type you want (1, 2)",
             exampletxt)
 
-        def isInputValid(self, input):
-            chartType = ""
-            if (input == "1"):
-                chartType = "Bar"
-                return True
-            elif (input == "2"):
-                chartType = "Line"
-                return True
-            else:
-                print("Please enter 1 or 2")
-                __init__
-                return False
+    def isInputValid(self, input):
+        if (input == "1"):
+            return True
+        elif (input == "2"):
+            return True
+        else:
+            print("Please enter 1 or 2")
+            return False
 
--- Constants.py
+++ Constants.py
@@ -5,3 +5,8 @@ class Constants:
     SYMBOL_SEARCH = "SYMBOL_SEARCH"
     API_URL = "https://www.alphavantage.co/query"
     API_KEY = "SPFP6JN8KMDZ9IPM"
+
+    CHARTTYPES = {
+        "1": "Bar",
+        "2": "Line"
+    }
