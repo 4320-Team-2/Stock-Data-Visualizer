ecd281969d6de2d50527dfd4e350447d9061f177
Nilin Reddrex <nilin.reddrex@outlook.com>
2020-10-28 19:39:29 -0700

    Integrated time series function into framework as user input. Updated stock data visualizer inputs to return simple dictionary of values.

--- StockDataVisualizerInputs.py
+++ StockDataVisualizerInputs.py
@@ -1,5 +1,6 @@
 from SymbolInput import SymbolInput
 from ChartTypeInput import ChartTypeInput
+from TimeSeriesInput import TimeSeriesInput
 from Constants import Constants
 
 class StockDataVisualizerInputs:
@@ -7,7 +8,8 @@ class StockDataVisualizerInputs:
         #TODO: Add input objects for remaining required inputs.
         self.userInputs = {
             Constants.SYMBOL: SymbolInput(),
-            Constants.CHARTTYPE: ChartTypeInput()
+            Constants.CHARTTYPE: ChartTypeInput(),
+            Constants.SERIES: TimeSeriesInput()
         }
 
     def gatherUserInput(self):
@@ -15,9 +17,9 @@ class StockDataVisualizerInputs:
         Using the required self.userInputs, prompts user for required inputs and validates provided values. 
         Valid inputs are stored in the respective self.userInputs[i].value 
         """
+        kvp = {}
         # For each user input object 
-        for field in self.userInputs:
-            ui = self.userInputs[field]
+        for key, ui in self.userInputs.items():
             while True:
                 #display the optional options txt to display valid options to the user
                 print("\r\n" + ui.optionsTxt)
@@ -32,3 +34,8 @@ class StockDataVisualizerInputs:
                 else:
                     # If input is invalid notify user and continue looping asking for valid input.
                     print("Value '" + userInput + "' is not valid.")
+            
+            # add validated value to return value
+            kvp.update({key: ui.value})
+        
+        return kvp
