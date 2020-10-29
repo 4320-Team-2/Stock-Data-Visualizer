24d094b1837b7f6d28000fc230a4def18422a3d3
Nilin Reddrex <nilin.reddrex@outlook.com>
2020-10-28 18:18:18 -0700

    Implemented symbol input validation.

--- Constants.py
+++ Constants.py
@@ -2,3 +2,6 @@ class Constants:
     # Use constants so specfic user input type can be referenced globally without magic strings everywhere.
     SYMBOL = "symbol"
     CHARTTYPE = "chartType"
+    SYMBOL_SEARCH = "SYMBOL_SEARCH"
+    API_URL = "https://www.alphavantage.co/query"
+    API_KEY = "SPFP6JN8KMDZ9IPM"
--- SymbolInput.py
+++ SymbolInput.py
@@ -1,5 +1,6 @@
 from UserInput import UserInput
-
+import requests 
+from Constants import Constants
 class SymbolInput(UserInput):
     
     def __init__(self):
@@ -8,9 +9,31 @@ class SymbolInput(UserInput):
             self, 
             "Enter the stock symbol you are looking for")
 
-    def isInputValid(self, input):
+    def isInputValid(self, symbol):
         """The shared validate input function."""
-        if (input == "Test"):
-            return True
-        else:
-            return False
+        # Query paramaters
+        data = {
+            "function":Constants.SYMBOL_SEARCH,
+            "keywords":symbol,
+            "apikey":Constants.API_KEY
+        }
+
+        # Query symbol search API
+        apiCall = requests.get(Constants.API_URL, params=data)
+        response = apiCall.json()
+
+        # for each returned match 
+        matches = []
+        for match in response["bestMatches"]:
+            # if an exact match symbol is valid; return true
+            if(match["1. symbol"].upper() == symbol.upper()):
+                return True
+            else:
+                matches.append(match["1. symbol"])
+            
+        # if not an exact match, display best matches from the api
+        print(f'{symbol} was not found. Did you mean {", ".join(matches)}?')
+        return False
+
+
+
